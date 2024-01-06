fd7d1aa313c820283edf5da39025622912482e22
normansimonr <normansimonr@gmail.com>
2024-01-06 17:36:40 -0500

    Create alignment table

--- scripts/apparatus_creator.py
+++ scripts/apparatus_creator.py
@@ -1,3 +1,6 @@
+import re
+import collatex
+import difflib
 import pandas as pd
 
 third_john_manuscript_attestation = {
@@ -114,7 +117,7 @@ for chapter in third_john_manuscript_attestation.keys():
         byz_verse = byz[(byz['chapter']==chapter) & (byz['verse']==verse)]['text'].to_list()
         
         if len(byz_verse) == 0:
-            byz_verse = ''
+            byz_verse = 'G'
         else:
             byz_verse = byz_verse[0]
         
@@ -128,17 +131,88 @@ for chapter in third_john_manuscript_attestation.keys():
         
         verse_attestation = pd.DataFrame(verse_attestation)
         verse_attestation.columns = ['manuscript_id', 'chapter', 'verse', 'parsed_greek']
+        verse_attestation['manuscript_id'] = verse_attestation['manuscript_id'].astype(str)
+        
+        # Grouping the DataFrame by specified columns and aggregating the manuscript_id column
+        grouped = verse_attestation.groupby(['chapter', 'verse', 'parsed_greek'])['manuscript_id']
+
+        # Applying aggregation functions to the grouped data
+        collation_for_this_verse = grouped.agg(manuscript_list=lambda x: list(x), manuscript_count=lambda x: len(set(x)))
+
+        # Resetting the index to convert the grouped structure back to a DataFrame
+        collation_for_this_verse = collation_for_this_verse.reset_index()
+        
+        # Determining if Byz is the majority reading
+        collation_for_this_verse['includes_byz'] = collation_for_this_verse['manuscript_list'].astype(str).str.contains('Byz')
+        collation_for_this_verse['includes_byz'] = collation_for_this_verse['includes_byz'].astype(int)
+        
+        collation_for_this_verse['manuscript_count'] = collation_for_this_verse['manuscript_count'] - collation_for_this_verse['includes_byz']
+        
+        count_manuscripts_attestation = collation_for_this_verse['manuscript_count'].sum()
+        
+        if (count_manuscripts_attestation % 2) == 0: # Determining the majority threshold depending on if the manuscript count is even or odd
+            majority_threshold = count_manuscripts_attestation/2
+        else:
+            majority_threshold = (count_manuscripts_attestation+1)/2
         
         
-        ### Collation
-        collation_for_this_verse = verse_attestation.groupby(['chapter', 'verse', 'parsed_greek'])['manuscript_id'].apply(list)
-        
-        is_byzantine_majority = 
         
         
-        print(verse_attestation)
-        print(collation_for_this_verse)
+        # Collation
+        # Cleaning the texts
+        def clean_text(text):
+            pattern_omitted = r'\[[^\]]+\]\{\.greek-omitted\}'
+            cleaned_string = re.sub(pattern_omitted, '', text)
+            pattern_supplied = r'\[[^\]]+\]\{\.greek-supplied\}'
+            cleaned_string = re.sub(pattern_supplied, 'U', cleaned_string)
+            pattern_unclear = r'\[[^\]]+\]\{\.unclear\}'
+            cleaned_string = re.sub(pattern_unclear, 'U', cleaned_string)
+            cleaned_string = cleaned_string.replace('{.greek-added}', '')
+            cleaned_string = cleaned_string.replace('{.greek-abbr}', 'A')
+            cleaned_string = cleaned_string.replace(']{.corr}', 'C')
+            cleaned_string = cleaned_string.replace('[―]{.gap}', '')
+            cleaned_string = cleaned_string.replace(']', '')
+            cleaned_string = cleaned_string.replace('[', '')
+            
+            # Removing repeated ? and spaces
+            cleaned_string = re.sub(r'U+', 'U', cleaned_string)
+            cleaned_string = re.sub(r' +', ' ', cleaned_string)
+            
+            return cleaned_string
         
+        collation_for_this_verse['parsed_greek_clean'] = collation_for_this_verse['parsed_greek'].apply(clean_text)
+        
+        collation = collatex.Collation()
+        
+        for index, row in collation_for_this_verse.iterrows():
+            collation.add_plain_witness(', '.join(str(item) for item in row['manuscript_list']), row['parsed_greek_clean'])
+        
+        collatex_output = collatex.collate(collation, layout="vertical", near_match=True, segmentation=False)
+        
+        def collatex_output_to_df(collatex_output):
+            collatex_output = str(collatex_output)
+            rows = collatex_output.split('\n')
+            data = []
+            for row in rows:
+                if row.startswith('|'):
+                    row = row.strip('|')
+                    row = [cell.strip() for cell in row.split('|') if cell.strip()]
+                    data.append(row)
+
+            # Create a DataFrame from the extracted data
+            df = pd.DataFrame(data[1:], columns=data[0])
+
+            return df
+        
+        alignment_table = collatex_output_to_df(collatex_output)
+        
+        for index, row in alignment_table.iterrows():
+            textual_unit = row.reset_index()
+            textual_unit.columns = ['manuscript_list', 'reading']
+            textual_unit = textual_unit.groupby('reading')['manuscript_list'].agg(list)
+            print(verse, textual_unit)
+        
+        #print(alignment_table)        
         
         collation_for_this_verse.to_csv(str(verse) + '_test.csv')
 
